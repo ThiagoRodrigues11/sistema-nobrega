b22e2d36c2796d5db701fab6aefe066568c7950d
Thiago <Thiagortaroco@gmail.com>
2025-05-08 14:14:44 -0300

    fix: adicionar plugin webpack para aliases

--- webpack.config.js
+++ webpack.config.js
@@ -61,4 +61,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ]
 };
