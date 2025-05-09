2e013fc8b35807f5e5705987ce49323e5ed6c889
Thiago <Thiagortaroco@gmail.com>
2025-05-09 12:24:11 -0300

    Adicionando dependências necessárias e ajustando configuração do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -103,7 +103,7 @@ module.exports = {
         }),
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: JSON.stringify('development')
+                NODE_ENV: JSON.stringify('production')
             }
         })
     ],
