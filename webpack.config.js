ead62d9b7c90f3261f798af2579eb9b2b0b4382a
Thiago <Thiagortaroco@gmail.com>
2025-05-09 11:44:04 -0300

    Atualizar configuração do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,78 +14,41 @@ module.exports = {
       '@': path.resolve(__dirname, 'src')
     },
     extensions: ['.js', '.jsx', '.json'],
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    fallback: {
-      path: require.resolve('path-browserify')
-    }
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    port: 10000,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'https://sistema-nobrega-1.onrender.com/api',
-        changeOrigin: true,
-        secure: false,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-        }
-      },
-    },
-    setupMiddlewares: (middlewares, devServer) => {
-      if (!devServer) {
-        throw new Error('webpack-dev-server is not defined');
-      }
-      
-      // Adicionando CORS middleware
-      const corsOptions = {
-        origin: true,
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-      };
-      middlewares.unshift(cors(corsOptions));
-      
-      return middlewares;
-    },
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
   },
   module: {
     rules: [
       {
-        test: /\.js$|\.jsx$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [
-              ['babel-plugin-module-resolver', {
-                root: [path.resolve(__dirname, 'src')],
-                alias: {
-                  '@': path.resolve(__dirname, 'src')
-                }
-              }]
-            ]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       },
       {
-        test: /\.css$/,
+        test: /\.(css|scss)$/,
         use: ['style-loader', 'css-loader']
       }
     ]
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  devServer: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: 'all',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    },
+    proxy: {
+      '/api': {
+        target: 'https://sistema-nobrega-1.onrender.com',
+        changeOrigin: true,
+        secure: false
       }
-    })
-  ]
+    }
+  }
 };
